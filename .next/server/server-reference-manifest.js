a74aeb8111ce569dad2d655244fcafdf4969a6d9
NicolasCandelaria <nicolascandelaria8@gmail.com>
2026-03-09 10:09:55 -0700

    updated dependencies for hosting

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6z9IfBC1HaWbaDUP+iwTUHP/sN6WbyPhHYA1UenQe5M=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
